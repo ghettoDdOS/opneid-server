0aeddb6358e093230474426f40c2d100e993b479
ghettoddos <pen.egor2002@gmai.com>
2024-11-12 17:15:57 +0300

    chore: dependencies

--- opneid_server/asgi.py
+++ opneid_server/asgi.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
+from fastapi.responses import ORJSONResponse
 
 from opneid_server.router import root_router
 
 
 def get_application():
-    return FastAPI(routes=root_router.routes)
+    return FastAPI(
+        routes=root_router.routes,
+        default_response_class=ORJSONResponse,
+    )
 
 
 application = get_application()
--- opneid_server/router.py
+++ opneid_server/router.py
@@ -1,8 +1,10 @@
+from typing import Literal
+
 from fastapi import APIRouter
 
 root_router = APIRouter()
 
 
-@root_router.get('/ping')
+@root_router.get('/ping', response_model=Literal['pong'])
 def ping():
     return 'pong'
